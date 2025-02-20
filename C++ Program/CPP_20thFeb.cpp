119041e6396d5473b781861c282b02f4d016018a
Pramod Devrath <pramod.devrath@gmail.com>
2025-02-21 05:13:34 +0530

    Norhing special

--- C++ Program/CPP_20thFeb.cpp
+++ C++ Program/CPP_20thFeb.cpp
@@ -2,6 +2,15 @@
 
 #include <iostream>
 using namespace std;
+class math {
+    int a,b;
+    public : 
+    math(int x, int y ) :a(x),b(y){}
+    int sum() {return a+b;}
+}   ;
 int main() {
+    math obj(5,6);
+    cout<<obj.sum();
+    
     return 0;
 }
